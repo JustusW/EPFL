3fc83cb023532c86fb17b9f62c568ae9f637630c
Justus Wingert <juw@solute.de>
2015-03-26 10:36:13 +0100

    Implemented example to show slow rendering.

--- epfl_pyramid_barebone/views/home.py
+++ epfl_pyramid_barebone/views/home.py
@@ -5,7 +5,24 @@ from solute import epfl
 
 
 class HomeRoot(epfl.components.CardinalLayout):
-    pass
+    def init_struct(self):
+        for x in range(0, 10):
+            node_list = []
+            for y in range(0, 100):
+                sub_node_list = []
+                for z in range(0, 1):
+                    sub_node_list.append(epfl.components.Box(
+                        title='Box %s in Box %s in Box %s' % (z, y, x)
+                    ))
+                node_list.append(epfl.components.Box(
+                    title='Box %s in Box %s' % (y, x),
+                    node_list=sub_node_list
+                ))
+            self.node_list.append(epfl.components.Box(
+                title='Box %s' % x,
+                node_list=node_list
+            ))
+
 
 @view_config(route_name='home')
 class HomePage(epfl.Page):
